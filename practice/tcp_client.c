4a4fb85d7c0951ef70394c33894127a5f6abb4b6
falguni8 <89798656+falguni8@users.noreply.github.com>
2023-01-16 23:06:42 +0530

    Create tcp_client.c

---
+++ practice/tcp_client.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+
+int main() {
+    //create a socket
+    int network_socket;
+    network_socket=socket(AF_INET,SOCK_STREAM,0);
+
+    //specify an address for the socket
+    struct sockaddr_in server_add;
+    server_add.sin_family=AF_INET;//ip4
+    server_add.sin_port=htons(9002);//port
+    server_add.sin_addr.s_addr=INADDR_ANY;
+
+    int connection_status=connect(network_socket,(struct sockaddr*)&server_add,sizeof(server_add));
+
+    if(connection_status<0){
+        printf("ERROR in connecting to socket\n");
+    }
+
+    //receive data from server
+
+    char message[256];
+    scanf("%s",message);
+    int l=strlen(message);
+    //send tto server
+    while(1){
+        if(strcmp(message,exit)){
+            break;
+        }
+        send(network_socket,message,sizeof(message),0);
+    }
+    close(network_socket);
+    return 0;
+
+}
